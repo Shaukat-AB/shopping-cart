18fe82462a5a0a64ca10938303dd8a15553e87f5
Shaukat Ali <Shaukatalibans@gmail.com>
2024-02-13 10:28:02 +0500

    Add button variant for icons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export const Button = ({
     variant = "default",
     color = "main",
     onClick = () => null,
-    className="",
+    className = "",
     btnProps = {},
     children,
 }) => {
@@ -15,16 +15,26 @@ export const Button = ({
             "text-red-700 border-red-700 hover:bg-red-800 focus:ring-red-300",
     };
 
+    const common =
+        "transition-colors focus:ring-4 focus:outline-none font-medium border text-sm";
+
     const variants = {
-        default:
-            "transition-colors text-white focus:ring-4 focus:outline-none font-medium border border-transparent rounded-lg text-sm px-5 py-2.5 me-2 mb-2",
-        outlined:
-            "transition-colors hover:text-white focus:ring-4 focus:outline-none font-medium border rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2",
+        default: `${common} text-white border-transparent rounded-lg px-5 py-2.5`,
+        outlined: `${common} hover:text-white rounded-lg px-5 py-2.5`,
+        icon: `${common} hover:text-white rounded-full px-2 py-2 disabled:bg-gray-100 disabled:text-gray-900 disabled:hover:text-gray-900 disabled:border-gray-100`,
     };
-    const finalStyle = (() =>
-        variant !== "outlined"
-            ? `${variants["default"]} ${colors[color]} ${className}`
-            : `${variants["outlined"]} ${colors[color + "outlined"]} ${className}`)();
+
+    const finalStyle = (() => {
+        const outlined = `${colors[color + "outlined"]} ${className}`;
+        switch (variant) {
+            case "outlined":
+                return `${variants["outlined"]} ${outlined}`;
+            case "icon":
+                return `${variants["icon"]} ${outlined}`;
+            default:
+                return `${variants["default"]} ${colors[color]} ${className}`;
+        }
+    })();
 
     return (
         <button className={finalStyle} onClick={() => onClick()} {...btnProps}>
